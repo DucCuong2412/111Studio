b3fcf8709942ed96f322346470eb212cc0a669fb
tiennam050505 <164200353+tiennam050505@users.noreply.github.com>
2024-09-20 12:11:25 +0700

    submit

---
+++ Assets/Scrip/SadowRun.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SadowRun : StateMachineBehaviour
+{
+    public float speed= 2.5f;
+
+
+    Transform player;
+    Rigidbody2D rb;
+    flipboss flipboss;
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo animatorState,int layerIndex)
+    {
+        player = GameObject.FindGameObjectWithTag("player").transform;
+        rb = animator.GetComponent<Rigidbody2D>();
+        flipboss = animator.GetComponent<flipboss>();
+
+    }
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo animatorState, int layerIndex)
+    {
+        flipboss.LookatPlayer();
+
+        Vector2 target = new Vector2(player.position.x, rb.position.y);
+        Vector2 newPos = Vector2.MoveTowards(rb.position, target, speed * Time.fixedDeltaTime);
+        rb.MovePosition(newPos);
+    }
+    override public void OnStateExit(Animator animator, AnimatorStateInfo animatorState, int layerIndex)
+    {
+
+    }
+
+}
