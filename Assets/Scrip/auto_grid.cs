9d2d2a1a773f54f9c5d7de0f9a07792306ac8fc2
DucCuong2412 <cuongduc2587@gmail.com>
2024-10-03 23:54:39 +0700

    submid

---
+++ Assets/Scrip/auto_grid.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class MoveBetweenTwoObjects : MonoBehaviour
+{
+    public GameObject pointA; // Đối tượng làm điểm A
+    public GameObject pointB; // Đối tượng làm điểm B
+    public float speed = 2.0f; // Tốc độ di chuyển
+
+    private bool movingToB = true; // Để theo dõi hướng di chuyển
+
+    void Update()
+    {
+        // Kiểm tra hướng di chuyển
+        if (movingToB)
+        {
+            // Di chuyển về phía điểm B
+            transform.position = Vector3.MoveTowards(transform.position, pointB.transform.position, speed * Time.deltaTime);
+
+            // Khi đối tượng đến gần điểm B, đổi hướng
+            if (Vector3.Distance(transform.position, pointB.transform.position) < 0.1f)
+            {
+                movingToB = false;
+            }
+        }
+        else
+        {
+            // Di chuyển về phía điểm A
+            transform.position = Vector3.MoveTowards(transform.position, pointA.transform.position, speed * Time.deltaTime);
+
+            // Khi đối tượng đến gần điểm A, đổi hướng
+            if (Vector3.Distance(transform.position, pointA.transform.position) < 0.1f)
+            {
+                movingToB = true;
+            }
+        }
+    }
+}
---
+++ Assets/Scrip/door.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class door : MonoBehaviour
+{
+    public GameObject door2;
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                collision.transform.position= door2.transform.position;
+            }
+
+            }
+
+    }
+}
