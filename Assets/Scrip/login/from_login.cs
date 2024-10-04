0ac97839c05450a1cb0a08a1e9edc05385732341
DucCuong2412 <cuongduc2587@gmail.com>
2024-10-05 01:23:10 +0700

    lab2

--- Assets/Scrip/login/from_login.cs
+++ Assets/Scrip/login/from_login.cs
@@ -1,9 +1,25 @@
-using UnityEngine;
+﻿using UnityEngine;
 [System.Serializable]
 public class login
 {
     public string id;
     public string password;
     public string againpasss;
+    public string hightScore1;
+    public string hightScore2;
+    public string hightScore3;
+    public string hightScore4;
+    public string hightScoreASM;
 
+    public login(string id, string password, string againpass)
+    {
+        this.id = id;
+        this.password = password;
+        this.againpasss = againpass;
+        this.hightScore1 = "0"; // Điểm cao mặc định
+        this.hightScore2 = "0"; // Điểm cao mặc định
+        this.hightScore3 = "0"; // Điểm cao mặc định
+        this.hightScore4 = "0"; // Điểm cao mặc định
+        this.hightScoreASM = "0"; // Điểm cao mặc định
+    }
 }
