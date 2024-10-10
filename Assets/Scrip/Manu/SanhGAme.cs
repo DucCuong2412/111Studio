e170d8588fdc3aa00927090eeb0e7b87924ebb1f
tiennam050505 <164200353+tiennam050505@users.noreply.github.com>
2024-10-11 04:00:11 +0700

    sanhMap 90%

---
+++ Assets/Scrip/Manu/SanhGAme.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SanhGAme : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    public void LoadScene(string index) 
+    { 
+        SceneManager.LoadScene(index);
+    }
+
+    
+}
