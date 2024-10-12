ebfbd3010bf5814937c4b9e3025e1198818881a5
tiennam050505 <164200353+tiennam050505@users.noreply.github.com>
2024-10-12 12:55:00 +0700

    bullet

--- Assets/Scrip/boss/ThayAnMove.cs
+++ Assets/Scrip/boss/ThayAnMove.cs
@@ -20,7 +20,6 @@ public class ThayAnMove : StateMachineBehaviour
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo animatorState, int layerIndex)
     {
         flipboss.LookatPlayer();
-
         Vector2 target = new Vector2(player.position.x, rb.position.y);
         Vector2 newPos = Vector2.MoveTowards(rb.position, target, speed * Time.fixedDeltaTime);
         rb.MovePosition(newPos);
